3b89664e52997ee14e980cf76aaf09c8f8f0ea70
ambakick <neerajmenons@gmail.com>
2018-07-03 23:25:07 +0530

    Adding conv

---
+++ Convolutional Nueral Nets/keras_demo.py
@@ -0,0 +1,99 @@
+#importing all the required packages
+
+import numpy as np
+from keras import layers
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from keras.models import Model
+from keras.preprocessing import image
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+import pydot
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+from kt_utils import *
+
+import keras.backend as K
+K.set_image_data_format('channels_last')
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+
+
+
+#getting dataset and divding it
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+# Normalize image vectors
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+
+# Reshape
+Y_train = Y_train_orig.T
+Y_test = Y_test_orig.T
+
+
+'''
+#optional
+#to see the dimensions of different matriX used
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+'''
+
+
+#before training it is required to create a model
+
+def HappyModel(input_shape):
+    """
+    Implementation of the HappyModel.
+    
+    Arguments:
+    input_shape -- shape of the images of the dataset
+
+    Returns:
+    model -- a Model() instance in Keras
+    """
+    
+
+    X_input = Input(input_shape)
+
+    # Zero-Padding: pads the border of X_input with zeroes
+    X = ZeroPadding2D((3, 3))(X_input)
+
+    # CONV -> BN -> RELU Block applied to X
+    X = Conv2D(32, (7, 7), strides = (1, 1), name = 'conv0')(X)
+    X = BatchNormalization(axis = 3, name = 'bn0')(X)
+    X = Activation('relu')(X)
+
+    # MAXPOOL
+    X = MaxPooling2D((2, 2), name='max_pool')(X)
+
+    # FLATTEN X (means convert it to a vector) + FULLYCONNECTED
+    X = Flatten()(X)
+    X = Dense(1, activation='sigmoid', name='fc')(X)
+
+    # Create model. This creates your Keras model instance, you'll use this instance to train/test the model.
+    model = Model(inputs = X_input, outputs = X, name='HappyModel')
+    
+    
+    return model
+
+#input size is solely dependent on the input image size 
+#here 3 denotes that it is rgb color image so if a grey scale image is the input then it becomes 1
+input1 = 64, 64, 3
+happyModel = HappyModel(input1)
+
+#happy model is the model that we created and gonna train
+
+happyModel.compile(optimizer = "adam",loss = "binary_crossentropy",metrics = ["accuracy"])
+#currently as implementing a classification problem binary_crossentropy used
+
+
+#training using keras is just single line
+#i still recommend implementing using tensorflow or lower level api to get the taste of what is going on
+happyModel.fit(x = X_train,y = Y_train,epochs =40 ,batch_size = 16 )
