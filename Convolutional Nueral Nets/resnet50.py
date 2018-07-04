5224e268868d46bce95f6b720fafe82bafec9731
ambakick <neerajmenons@gmail.com>
2018-07-04 20:51:49 +0530

    resent50

--- Convolutional Nueral Nets/resnet50.py
+++ Convolutional Nueral Nets/resnet50.py
@@ -75,6 +75,7 @@ def identity_block(X, f, filters, stage, block):
     
     return X
 
+#when the input size is not equal to the output size then there is requirement of conv layer in the shortcut so that the matriX dimensions can be matched
 def convolutional_block(X, f, filters, stage, block, s = 2):
     """
     Implementation of the convolutional block as defined in Figure 4
@@ -130,3 +131,126 @@ def convolutional_block(X, f, filters, stage, block, s = 2):
     ### END CODE HERE ###
     
     return X
+
+#creating the resnet model
+'''
+Model structure
+
+Zero-padding pads the input with a pad of (3,3)
+Stage 1:
+The 2D Convolution has 64 filters of shape (7,7) and uses a stride of (2,2). Its name is "conv1".
+BatchNorm is applied to the channels axis of the input.
+MaxPooling uses a (3,3) window and a (2,2) stride.
+Stage 2:
+The convolutional block uses three set of filters of size [64,64,256], "f" is 3, "s" is 1 and the block is "a".
+The 2 identity blocks use three set of filters of size [64,64,256], "f" is 3 and the blocks are "b" and "c".
+Stage 3:
+The convolutional block uses three set of filters of size [128,128,512], "f" is 3, "s" is 2 and the block is "a".
+The 3 identity blocks use three set of filters of size [128,128,512], "f" is 3 and the blocks are "b", "c" and "d".
+Stage 4:
+The convolutional block uses three set of filters of size [256, 256, 1024], "f" is 3, "s" is 2 and the block is "a".
+The 5 identity blocks use three set of filters of size [256, 256, 1024], "f" is 3 and the blocks are "b", "c", "d", "e" and "f".
+Stage 5:
+The convolutional block uses three set of filters of size [512, 512, 2048], "f" is 3, "s" is 2 and the block is "a".
+The 2 identity blocks use three set of filters of size [512, 512, 2048], "f" is 3 and the blocks are "b" and "c".
+The 2D Average Pooling uses a window of shape (2,2) and its name is "avg_pool".
+The flatten doesn't have any hyperparameters or name.
+The Fully Connected (Dense) layer reduces its input to the number of classes using a softmax activation. Its name should be 'fc' + str(classes).
+'''
+def ResNet50(input_shape = (64, 64, 3), classes = 6):
+    """
+    Implementation of the popular ResNet50 the following architecture:
+    CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK*2 -> CONVBLOCK -> IDBLOCK*3
+    -> CONVBLOCK -> IDBLOCK*5 -> CONVBLOCK -> IDBLOCK*2 -> AVGPOOL -> TOPLAYER
+
+    Arguments:
+    input_shape -- shape of the images of the dataset
+    classes -- integer, number of classes
+
+    Returns:
+    model -- a Model() instance in Keras
+    """
+    
+    # Define the input as a tensor with shape input_shape
+    X_input = Input(input_shape)
+
+    
+    # Zero-Padding
+    X = ZeroPadding2D((3, 3))(X_input)
+    
+    # Stage 1
+    X = Conv2D(64, (7, 7), strides = (2, 2), name = 'conv1', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    # Stage 2
+    X = convolutional_block(X, f = 3, filters = [64, 64, 256], stage = 2, block='a', s = 1)
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+
+    ### START CODE HERE ###
+
+    # Stage 3 (≈4 lines)
+    X = convolutional_block(X, f = 3, filters = [128, 128, 512], stage = 3, block='a', s = 2)
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='b')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='c')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='d')
+
+    # Stage 4 (≈6 lines)
+    X = convolutional_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block='a', s = 2)
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
+
+    # Stage 5 (≈3 lines)
+    X = convolutional_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block='a', s = 2)
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
+
+    # AVGPOOL (≈1 line). Use "X = AveragePooling2D(...)(X)"
+    X = AveragePooling2D()(X)
+    
+    ### END CODE HERE ###
+
+    # output layer
+    X = Flatten()(X)
+    X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=0))(X)
+    
+    
+    # Create model
+    model = Model(inputs = X_input, outputs = X, name='ResNet50')
+
+    return model
+
+#sample input to the model
+model = ResNet50(input_shape = (64, 64, 3), classes = 6)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+#training
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+# Normalize image vectors
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+
+# Convert training and test labels to one hot matrices
+Y_train = convert_to_one_hot(Y_train_orig, 6).T
+Y_test = convert_to_one_hot(Y_test_orig, 6).T
+'''
+#optional
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+'''
+#training step
+model.fit(X_train, Y_train, epochs = 2, batch_size = 32)
+
+preds = model.evaluate(X_test, Y_test)
+print ("Loss = " + str(preds[0]))
+print ("Test Accuracy = " + str(preds[1]))
