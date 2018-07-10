94f80fdaad95cd77c3835c0426be52dad35f6088
ambakick <neerajmenons@gmail.com>
2018-07-10 19:19:29 +0530

    iou

--- Convolutional Nueral Nets/yolo.py
+++ Convolutional Nueral Nets/yolo.py
@@ -60,3 +60,36 @@ def yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold = .6):
     ### END CODE HERE ###
     
     return scores, boxes, classes
+
+# GRADED FUNCTION: iou
+
+def iou(box1, box2):
+    """Implement the intersection over union (IoU) between box1 and box2
+    
+    Arguments:
+    box1 -- first box, list object with coordinates (x1, y1, x2, y2)
+    box2 -- second box, list object with coordinates (x1, y1, x2, y2)
+    """
+
+    # Calculate the (y1, x1, y2, x2) coordinates of the intersection of box1 and box2. Calculate its Area.
+    ### START CODE HERE ### (≈ 5 lines)
+    xi1 = np.max([box1[0],box2[0]])
+    yi1 = np.max([box1[1],box2[1]])
+    xi2 = np.min([box1[2],box2[2]])
+    yi2 = np.min([box1[3],box2[3]])
+    inter_area = max(yi2-yi1, 0)*max(xi2-xi1, 0)
+    ### END CODE HERE ###    
+
+    # Calculate the Union area by using Formula: Union(A,B) = A + B - Inter(A,B)
+    ### START CODE HERE ### (≈ 3 lines)
+    box1_area = (box1[3]-box1[1])*(box1[2]-box1[0])
+    box2_area = (box2[3]-box2[1])*(box2[2]-box2[0])
+    union_area = box1_area + box2_area - inter_area
+    ### END CODE HERE ###
+    
+    # compute the IoU
+    ### START CODE HERE ### (≈ 1 line)
+    iou = inter_area/union_area
+    ### END CODE HERE ###
+    
+    return iou
